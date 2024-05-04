a59ad70ab0f17bdf2696b0cd913515939a4ae0a4
Chris <chris@christian-nieber.de>
2024-05-04 10:39:47 +0100

    run_nas.sh returncode handling

--- run_nas.sh
+++ run_nas.sh
@@ -4,7 +4,7 @@ while true; do
   python -u main_20_runs.py
   ret_code=$?
   echo 'PROCESS RETURNED' $ret_code
-  if [ $ret_code == 0 ] || [ $ret_code == 1 ]; then
+  if [ $ret_code == 0 ] || [ $ret_code == 123 ]; then
       exit 0
   fi
   echo 'RESTARTING PROCESS'
